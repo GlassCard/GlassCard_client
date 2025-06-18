d8e2a687bf10ad3af8680537f3fb5b95babe7a35
winshine0326 <shane10340@gmail.com>
2025-06-18 11:03:55 +0900

    feat :: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Layout from "./layout"
+import Home from "./pages/home"
 
 function App() {
 
   return (
     <>
-    ㅇㅇ
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>} />
+      </Route>
+    </Routes>
     </>
   )
 }
---
+++ src/layout/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const Layout = () => {
+    return(
+        <>
+            This is Layout
+            <Outlet />
+        </>
+    );
+}
+
+export default Layout;
