d1e434d1fb9baa4ef49a3dd3fdc013c194a32b39
winshine0326 <shane10340@gmail.com>
2025-06-22 15:41:04 +0900

    feat :: create home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,26 @@
 import Header from "@/components/header";
+import * as _ from './style';
+import { Link } from "react-router-dom";
 
 const Home = () =>{
     return(
         <>
             <Header />
+            <_.Card>
+                <_.TitleContainer>
+                    <_.Title>AI가 도와주는 <_.Highlight>똑똑한</_.Highlight> 단어 학습</_.Title>
+                </_.TitleContainer>
+                <_.InnerCard>
+                    <_.InnerCardText>정확한 답만 인정하는 경직된 학습에서 벗어나,</_.InnerCardText>
+                    <_.InnerCardText>AI 유사도 분석으로 더 <_.Highlight>자연스럽고 효과적인</_.Highlight></_.InnerCardText>
+                    <_.InnerCardText>단어 암기를 경험하세요.</_.InnerCardText>
+                </_.InnerCard>
+                <Link to={"/card-list"} style={{ all: 'unset', cursor: 'pointer', width: '100%'}}>
+                    <_.StartBtn>
+                        <_.StartBtnText>시작하기</_.StartBtnText>
+                    </_.StartBtn>
+                </Link>
+            </_.Card>
         </>
     )
 }
---
+++ src/pages/home/style.ts
@@ -0,0 +1,99 @@
+import styled from "@emotion/styled";
+
+export const Card = styled.div`
+    display: flex;
+    width: 55%;
+    max-width:868px;
+    margin: auto;
+    padding: 64px 32px;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    height:100%;
+    border-radius: 64px;
+    border: 8px solid var(--background-surface);
+    background: var(--background-fill);
+
+    /* 글래스처리 */
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(40px);
+`
+
+export const TitleContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+    background-color:transparent;
+`
+
+export const Title = styled.p`
+    color: var(--primary-darker);
+    text-align: center;
+
+    font-family: "Pretendard Variable";
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 120%;
+    background-color:transparent;
+`
+
+export const InnerCard = styled.div`
+    display: flex;
+    height: 246px;
+    padding: 24px 10px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+    border-radius: var(--64px, 64px);
+    border: 4px solid var(--background-surface);
+    background: var(--background-fill);
+
+    /* 글래스 인 글래스 */
+    box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(50px);
+`
+
+export const InnerCardText = styled.p`
+    color: var(--primary-darker);
+
+    font-family: "Pretendard Variable";
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 120%;
+    background-color:transparent;
+`
+
+export const StartBtn = styled.div`
+    display: flex;
+    padding: 24px 10px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+    border-radius: 64px;
+    border: 4px solid var(--background-surface);
+    background: rgba(45, 49, 87, 0.90);
+    cursor: pointer;
+`
+
+export const StartBtnText = styled.p`
+    color: var(--primary-light);
+
+    font-family: "Pretendard Variable";
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 120%;
+    background-color:transparent;
+`
+
+export const Highlight = styled.span`
+    color: var(--primary-dark);
+`
